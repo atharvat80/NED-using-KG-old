633be51f2669284126dcc4457234d79034a11ca6
Atharva <51092847+atharvat80@users.noreply.github.com>
2021-11-18 19:11:55 +0000

    Revert "Added files for colab"
    This reverts commit b8a5f0587a279c9d307b6a51b34454f269ef3a03.

--- embeddings/download.py
+++ embeddings/download.py
@@ -1,21 +1,10 @@
-import os
-import urllib
 import gensim.downloader as api
 
-# Load models from 
-print("Downloading wikipedia2vec...")
-urllib.urlretrieve(
-	"http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_win10_100d.txt.bz2", 
-	"wiki2vec_w10_100d.txt.bz2"
-)
-print("Extracting downloaded file...")
-os.system("bzip2 -d wiki2vec_w10_100d.txt.bz2")
-print("Done.")
+model = api.load("fasttext-wiki-news-subwords-300")
+model.save("fasttext-wiki-news-subwords-300", pickle_protocol=4)
 
-# Load models about gensim
-models = ["fasttext-wiki-news-subwords-300", "glove-wiki-gigaword-300", "word2vec-google-news-300",]
-for i in models:
-	print("Loading", i)
-	model = api.load(i)
-	model.save(i, pickle_protocol=4)
-	print("Done.")
+model = api.load("glove-wiki-gigaword-300")
+model.save("glove-wiki-gigaword-300", pickle_protocol=4)
+
+model = api.load("word2vec-google-news-300")
+model.save("word2vec-google-news-300", pickle_protocol=4)
